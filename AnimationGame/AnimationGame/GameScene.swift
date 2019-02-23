3a6dcd1b64dea125df8caa93c1a2f4eea5a41793
Kevin Andrade <kandrad2@binghamton.edu>
2019-02-22 23:45:06 -0500

    added energy bullet with firing animation on touch

--- AnimationGame/AnimationGame/GameScene.swift
+++ AnimationGame/AnimationGame/GameScene.swift
@@ -10,7 +10,8 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    let player = SKSpriteNode(imageNamed: "playerShip")
+
     //This function loads everytime the scene loads up
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
@@ -19,7 +20,6 @@ class GameScene: SKScene {
         background.zPosition = 0    //order in layer
         self.addChild(background)   //take the code above and make it!
         
-        let player = SKSpriteNode(imageNamed: "playerShip")
         player.setScale(1)  //normal size, if bigger increase the number...each integer is twice as big as original
         player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)  //this will make the player spawn from 20 percent of the screen
         player.zPosition = 2
@@ -28,5 +28,27 @@ class GameScene: SKScene {
         
         
         
+    }
+    
+    
+    func fireBullet(){
+        //spawn bullet
+        let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.setScale(1)
+        bullet.position = player.position
+        bullet.zPosition = 1
+        self.addChild(bullet)
+        
+        //moving bullet up
+        let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1) //will take one second to move up the screen
+        let deleteBullet = SKAction.removeFromParent()
+        let bulletSequence = SKAction.sequence([moveBullet, deleteBullet])  //this takes an array of acions in sequence.
+        bullet.run(bulletSequence)
+        
+    
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fireBullet()
     }
 }
